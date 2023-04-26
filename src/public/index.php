df83716c5fec25635a04965c3d5bdaac86c84c24
Your Name <you@example.com>
2023-04-26 11:09:44 -0300

    primeiro commit

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,39 @@
 <?php
+//$time=array('maria','jose','ana');
+//echo $time[0];
+//echo'<br/>';
 
-phpinfo();
+//$nome='maria';
+//$idade=30;
+//print_r($time);
+//echo'a:['.TRUE.']<br/>';
+//echo'a:['.FALSE.']<br/>';
+
+//echo'a:['.73.']<br/>';
+//echo'b:['.'oi'.']<br/>';
+//echo'c:['.falso.']<br/>';
+//echo'd:['.$nome.']<br/>';
+//echo'e:['.$idade.']<br/>';
+//
+$saldo=10;
+if ($saldo<1000){
+    $deposito=1000;
+    $saldo+=$deposito;
+}
+ echo$saldo;
+
+
+
+//declare(strict_types=1);
+// phpinfo();
+//$contador=1;
+//$mensagem="ola";
+//$versao=array('1.1','2','3.4');
+//print_r($versao);
+//primeiro_nome="maria";
+//echo $primeiro_nome;
+//echo'<br/>';
+//$ultimo_nome="loudres";
+//echo $ultimo_nome;
+
+?>
